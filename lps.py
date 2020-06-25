7150387690204c0ed81ee59dbe5814127467d962
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-06-24 22:38:05 -0400

    Orient objects differently, improve display,

--- lps.py
+++ lps.py
@@ -24,12 +24,6 @@ def lineIntersect(a, b, c, d):
 
     return (xnum / denom, ynum / denom)
 
-def getGridPosition(corners, x, y):
-    return lineIntersect((1-y)*corners[0][0]+y*corners[3][0],
-                         (1-y)*corners[1][0]+y*corners[2][0],
-                         (1-x)*corners[0][0]+x*corners[1][0],
-                         (1-x)*corners[3][0]+x*corners[2][0])
-
 def getTransformMatrices(corners, size):
     def mapBasisToPoints(points):
         homoPoints = [np.array([[point[0]], [point[1]], [1]]) for point in points]
@@ -43,7 +37,7 @@ def getTransformMatrices(corners, size):
                                                   [0, 0, coefficients[2][0]]]))
         return scaled
 
-    sceneCorners = ((-size/2, -size/2), (-size/2, size/2), (size/2, size/2), (size/2, -size/2))
+    sceneCorners = ((size/2, size/2), (-size/2, size/2), (-size/2, -size/2), (size/2, -size/2))
 
     basisToPicture = mapBasisToPoints(corners)
     basisToScene = mapBasisToPoints(sceneCorners)
@@ -64,7 +58,7 @@ def applyTransform(source_point, matrix):
     return dest_point
 
 def matchColor(color):
-    colors = {"white": (255, 128, 128),
+    colors = {"green": (128, 42, 211),
               "red": (128, 208, 195)}
 
 
@@ -98,18 +92,18 @@ class Marker:
         # create a "bootstrap" transform matrix, use this to read the squares
         # and determine the orientation, then create the "real" transform matrix.
 
-        pictureToScene, sceneToPicture = self.getTransformMatrices()
+        self.pictureToScene, self.sceneToPicture = self.getTransformMatrices()
 
-        boundary_points_scene = [((-1, -1), (0, -1), (0, 0), (-1, 0)),
-                                 ((0, -1), (1, -1), (1, 0), (0, 0)),
-                                 ((0, 0), (1, 0), (1, 1), (0, 1)),
-                                 ((-1, 0), (0, 0), (0, 1), (-1, 1))]
+        boundary_points_scene = [((1, 1), (0, 1), (0, 0), (1, 0)),
+                                 ((0, 1), (-1, 1), (-1, 0), (0, 0)),
+                                 ((0, 0), (-1, 0), (-1, -1), (0, -1)),
+                                 ((1, 0), (0, 0), (0, -1), (1, -1))]
 
         boundary_points = []
         for square in boundary_points_scene:
             boundary_points_square = []
             for scene_point in square:
-                picture_point = applyTransform(scene_point, sceneToPicture)
+                picture_point = applyTransform(scene_point, self.sceneToPicture)
                 # homo_scene_point = np.array([[coord] for coord in scene_point] + [[1]])
                 # homo_picture_point = np.matmul(sceneToPicture, homo_scene_point)
                 # picture_point = (int(homo_picture_point[0][0] / homo_picture_point[2][0]),
@@ -136,30 +130,59 @@ class Marker:
             mean = cv2.mean(image_lab, coord_masks[i])
             self.squares.append(matchColor(mean))
 
-    def findType(self):
-        if self.squares.count("red") == 1 and self.squares.count("white") == 3:
+        # Use the information gathered from the squares to interpret the type of this marker
+
+        if self.squares.count("red") == 1 and self.squares.count("green") == 3:
             self.type = "target"
             first_corner_index = self.squares.index("red")
-        elif self.squares.count("red") == 3 and self.squares.count("white") == 1:
+        elif self.squares.count("red") == 3 and self.squares.count("green") == 1:
             self.type = "reference"
-            first_corner_index = self.squares.index("white")
+            first_corner_index = self.squares.index("green")
         else:
             self.type = "none"
             first_corner_index = 0
+
         self.coords = np.concatenate((self.coords[first_corner_index:], self.coords[:first_corner_index]))
         self.squares = self.squares[first_corner_index:] + self.squares[:first_corner_index]
 
+        # Now that we have the orientation, recalculate the transform matrices
+        self.pictureToScene, self.sceneToPicture = self.getTransformMatrices()
+
     def getPicturePos(self):
-        pictureToScene, sceneToPicture = self.getTransformMatrices()
-        return applyTransform((0, 0), sceneToPicture)
-        #return self.coords[0][0]
+        return applyTransform((0, 0), self.sceneToPicture)
 
     def getGlobalPos(self, pictureToScene):
         return applyTransform(self.getPicturePos(), pictureToScene)
 
+    def display(self, image):
+        if self.type == "reference":
+            # Draw border
+            cv2.polylines(image, [self.coords], True, (255, 0, 255), 2)
+            # Draw center
+            cv2.circle(image, tuple(int(coord) for coord in self.getPicturePos()), 10, (255, 0, 255), 2)
+            # Draw X/Y axes
+            cv2.arrowedLine(image, tuple(int(coord) for coord in self.getPicturePos()),
+                            tuple(int(coord) for coord in applyTransform((1, 0), self.sceneToPicture)),
+                            (0, 0, 255), 2)
+            cv2.arrowedLine(image, tuple(int(coord) for coord in self.getPicturePos()),
+                            tuple(int(coord) for coord in applyTransform((0, 1), self.sceneToPicture)),
+                            (0, 255, 0), 2)
+        elif self.type == "target":
+            # Draw border
+            cv2.polylines(image, [self.coords], True, (0, 255, 255), 2)
+            # Draw center
+            cv2.circle(image, tuple(int(coord) for coord in self.getPicturePos()), 10, (0, 255, 255), 2)
+        else:
+            # Draw border
+            cv2.polylines(image, [self.coords], True, (0, 0, 255), 2)
+            # Draw center
+            cv2.circle(image, tuple(int(coord) for coord in self.getPicturePos()), 10, (0, 0, 255), 2)
+
 
 def findMarkers(image):
-    image_blur = cv2.GaussianBlur(image, (5, 5), 0)
+    image_blur = cv2.GaussianBlur(image, (15, 15), 0)
+
+    cv2.imshow("Blur", image_blur)
     image_hsv = cv2.cvtColor(image_blur, cv2.COLOR_BGR2HSV)
 
     mask = cv2.inRange(image_hsv, BLUE_MIN, BLUE_MAX)
@@ -170,21 +193,22 @@ def findMarkers(image):
     markers = []
 
     for contour in contours:
+        #cv2.drawContours(image, [contour], -1, (0, 255, 0), 2)
+
         length = cv2.arcLength(contour, True)
 
-        if length < 100:
+        if length < 50:
             continue
 
-        epsilon = 0.02*length
+        epsilon = 0.03*length
         approx = cv2.approxPolyDP(contour, epsilon, True)
+
+        #cv2.drawContours(image, [approx], -1, (255, 0, 0), 2)
 
         if len(approx) != 4:
             continue
 
-        cv2.drawContours(image, [contour], -1, (0, 255, 0), 2)
-        cv2.drawContours(image, [approx], -1, (255, 0, 0), 2)
-
-        #cv2.circle(image, tuple(*approx[0]), 10, (0, 0, 255), 2)
+        # cv2.circle(image, tuple(*approx[0]), 10, (0, 0, 255), 2)
 
         markers.append(Marker(approx))
 
@@ -202,7 +226,6 @@ while True:
 
     for marker in markers:
         marker.scanSquares(image)
-        marker.findType()
 
     # Sort markers into reference and targets based on number of red squares
 
@@ -213,7 +236,7 @@ while True:
             reference = marker
             break
 
-    # Construct projective transform (TODO)
+    # Construct projective transform
     if reference is not None:
         pictureToScene, sceneToPicture = reference.getTransformMatrices()
 
@@ -225,9 +248,9 @@ while True:
             global_pos = tuple(int(coord) for coord in marker.getGlobalPos(pictureToScene))
         else:
             global_pos = "NO REF"
-            print(marker.squares)
 
-        cv2.putText(image, f"{global_pos}:{marker.type}:{marker.squares}", tuple(int(coord) for coord in picture_pos), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0, 255), 3)
+        marker.display(image)
+        cv2.putText(image, f"{global_pos}", tuple(int(coord) for coord in picture_pos), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255, 0), 3)
 
     cv2.imshow("Frame", image)
 
