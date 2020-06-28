136ac0baa6a881d603796c06f15fb5e30e8deb78
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-06-28 01:07:50 -0400

    smooth positions as they come in

--- smooth.py
+++ smooth.py
@@ -6,9 +6,18 @@ import numpy as np
 import cv2
 
 targets = {}
+target_timestamps = {}
 next_letter = "A"
 
 labels = {}
+label_timestamps = {}
+
+
+def smooth_pos(old_pos, new_pos):
+    OLD_F = 0.9
+    NEW_F = 1 - OLD_F
+    return ((OLD_F*old_pos[0] + NEW_F*new_pos[0]),
+            (OLD_F*old_pos[1] + NEW_F*new_pos[1]))
 
 
 def target_letter():
@@ -39,17 +48,26 @@ def register(marker):
 
         for letter, target in targets.items():
             dist = scipy.spatial.distance.euclidean(marker.global_pos(),
-                                                    target.global_pos())
+                                                    target)
             if dist < smallest_dist:
                 smallest_dist = dist
                 closest_target = letter
 
         if closest_target is None:
             closest_target = target_letter()
-        targets[closest_target] = marker
+            targets[closest_target] = marker.global_pos()
+        else:
+            targets[closest_target] = smooth_pos(targets[closest_target],
+                                                 marker.global_pos())
+        target_timestamps[closest_target] = time.time()
 
     elif marker.type == "label":
-        labels[marker.num] = marker
+        if marker.num in labels:
+            labels[marker.num] = smooth_pos(labels[marker.num],
+                                            marker.global_pos())
+        else:
+            labels[marker.num] = marker.global_pos()
+        label_timestamps[marker.num] = time.time()
 
 
 def prune(threshold=0.2):
@@ -57,21 +75,22 @@ def prune(threshold=0.2):
     now = time.time()
 
     dead_targets = []
-    for letter, target in targets.items():
-        if now - target.timestamp > threshold:
-            print(f"purging {letter}")
+    for letter, timestamp in target_timestamps.items():
+        if now - timestamp > threshold:
             dead_targets.append(letter)
 
     for letter in dead_targets:
         del targets[letter]
+        del target_timestamps[letter]
 
     dead_labels = []
-    for label in labels.values():
-        if now - label.timestamp > threshold:
-            dead_labels.append(label.num)
+    for num, timestamp in label_timestamps.items():
+        if now - timestamp > threshold:
+            dead_labels.append(num)
 
     for num in dead_labels:
         del labels[num]
+        del label_timestamps[num]
 
 
 def render():
@@ -88,18 +107,16 @@ def render():
     cv2.arrowedLine(image, coord_to_px((0, 0)), coord_to_px((3, 0)),
                     (0, 255, 0), 2)
 
-    for letter, target in targets.items():
-        pos = target.global_pos()
+    for letter, pos in targets.items():
         if pos is not None:
             cv2.circle(image, coord_to_px(pos), 10, (0, 255, 255))
             cv2.putText(image, str(letter), coord_to_px(pos),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0, 0), 3)
 
-    for label in labels.values():
-        pos = label.global_pos()
+    for num, pos in labels.items():
         if pos is not None:
             cv2.circle(image, coord_to_px(pos), 10, (255, 255, 0))
-            cv2.putText(image, str(label.num), coord_to_px(pos),
+            cv2.putText(image, str(num), coord_to_px(pos),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0, 0), 3)
 
     return image
